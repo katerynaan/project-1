179b37b66955a19acc99f56d9f718517c59974ef
katerynaan <katerynaan@wix.com>
2022-10-17 16:31:21 +0300

    some commit for the main

--- src/scripts/koli/koli.ts
+++ src/scripts/koli/koli.ts
@@ -8,7 +8,7 @@ export default class Koli {
         const div = document.createElement("div");
         div.className = "koli-wrapper";
         div.innerHTML = `
-        <h1>Hello world from Koli Component in main, but not feature!</h1>
+        <h1>Hello world from Koli Component in main, but not feature, before rebase!</h1>
         `
         this.root.append(div);
     }
